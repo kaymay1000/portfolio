57d2e5ea5a32fe8d14b1b20e318638438231caa9
Kaylee Alvarado <kaymay1000@gmail.com>
2016-11-17 21:25:05 -0800

    Basic project scaffolding, created Project constructor function, started coming up with properties for Project objects.

---
+++ projectInfo.js
@@ -0,0 +1,20 @@
+var projectInfo = [
+  {
+    title: 'Guessing Game',
+    dateCreated: '07-10-2016',
+    image: '', //create imgs folder... put image filepath here.
+    fileSource: '', //project filepath
+  },
+  {
+    title: 'Campfire Coffee',
+    dateCreated: '08-10-2016',
+    image: '',
+    fileSource: '',
+  },
+  {
+    title: 'Bus Mall',
+    dateCreated: '08-20-2016',
+    image: '',
+    fileSource: '',
+  },
+];
