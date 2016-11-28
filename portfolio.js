89a6d24cbff113233380c1977353a1caa2d271f6
Kaylee Alvarado <kaymay1000@gmail.com>
2016-11-27 21:38:49 -0800

    Handlebars script added, and HTML template contains Handlebars syntax... Project object properties not yet populating template.

--- portfolio.js
+++ portfolio.js
@@ -2,23 +2,27 @@ var projects = [];
 
 var portfolio = {};
 
-function Project (title, dateCreated, image, fileSource) {
+function Project (title, dateCreated, image, description) {
   this.title = title;
   this.dateCreated = dateCreated;
   this.image = image;
-  this.fileSource = fileSource;
+  this.description = description;
+  // this.fileSource = fileSource;
   projects.push(this);
 }
 
 Project.prototype.toHtml = function() {
-  var $newProject = $('#projects').clone();
-  $newProject.find('a').text(this.title);
-  $newProject.attr('.date-created', this.dateCreated);
-  $newProject.attr('src', this.image);
-  $newProject.attr('.description', this.description);
-  // $newProject.attr('href', this.fileSource); //need to chain .html jQ method on?
-  $newProject.removeClass('template');
-  return $newProject;
+  // var $newProject = $('#projects').clone();
+  // $newProject.find('a').text(this.title);
+  // $newProject.attr('.date-created', this.dateCreated);
+  // $newProject.attr('src', this.image);
+  // $newProject.attr('.description', this.description);
+  // // $newProject.attr('href', this.fileSource); //need to chain .html jQ method on?
+  // $newProject.removeClass('template');
+  // return $newProject;
+  var htmlTemp = $('.template').html();
+  var temp = Handlebars.compile(htmlTemp); //Handlebars.compile returns a function, so we store it in a variable, pass 'this' into it (next line), and return it.
+  return temp(this);
 };
 
 portfolio.handleMainNav = function() {
