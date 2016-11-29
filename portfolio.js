d93ecbfad439ca587a11df3020c8680a76635f67
Kaylee Alvarado <kaymay1000@gmail.com>
2016-11-29 15:41:02 -0800

    Got Handlebars working properly

--- portfolio.js
+++ portfolio.js
@@ -12,7 +12,16 @@ function Project (obj) {
 }
 
 Project.prototype.toHtml = function() {
-  var htmlTemp = $('#template').html();
+  // var $newProject = $('.project-template').clone();
+  // $newProject.find('#title').text(this.title);
+  // $newProject.find('#date-created').text(this.dateCreated);
+  // $newProject.find('img').attr('src', this.image);
+  // $newProject.find('#description').text(this.description);
+  // // $newProject.attr('href', this.fileSource); //need to chain .html jQ method on?
+  // $newProject.removeClass('.project-template');
+  // return $newProject;
+
+  var htmlTemp = $('#template').html(); //#template refers to the ID of the Handlebars script tag right above the HTML template
   var temp = Handlebars.compile(htmlTemp); //Handlebars.compile returns a function, so we store it in a variable, pass 'this' into it (next line), and return it.
   return temp(this);
 };
